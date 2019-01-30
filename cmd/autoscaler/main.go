2ee9937422ab237958c74814eacdac662fc96a4b
Pavel <pavelr3@gmail.com>
2019-01-30 12:19:16 +0200

    Metrics group kind to be passed through cli

--- cmd/autoscaler/main.go
+++ cmd/autoscaler/main.go
@@ -17,6 +17,7 @@ func main() {
 	scaleInterval := flag.Duration("scale-interval", time.Minute, "Interval to call check scale function")
 	scaleWindow := flag.Duration("scale-window", time.Minute, "Time window after initial value to act upon")
 	metricsInterval := flag.Duration("metrics-poll-interval", time.Minute, "Interval to poll custom metrics")
+	metricsGroupKind := flag.String("metrics-group-kind", "", "Metrics resource kind")
 	metricName := flag.String("metric-name", "", "Metric name from custom metrics")
 	scaleThreshold := flag.Int64("scale-threshold", 0, "Maximum allowed value for metric to be considered below active")
 	flag.Parse()
@@ -29,7 +30,8 @@ func main() {
 		*scaleWindow,
 		*metricName,
 		*scaleThreshold,
-		*metricsInterval); err != nil {
+		*metricsInterval,
+		*metricsGroupKind); err != nil {
 		errors.PrintErrorStack(os.Stderr, err, 5)
 
 		os.Exit(1)
